4d4bb78b79260fa6181cf35650537b7ca1a10770
FabricioV <fabricionicolasviera@hotmail.com>
2021-06-21 15:17:21 -0300

    eliminacion categoria desde fachada

--- Dominio/Fachada.cs
+++ Dominio/Fachada.cs
@@ -7,33 +7,8 @@ namespace Dominio
 {
     public class Fachada
     {
-        #region Metodos de Categoria
+        
 
-        public static bool Categoria_Agregar(Common.Clases.Categoria pCategoria)
-        {
-            return Dominio.Clases.Categoria.Agregar(pCategoria);
-        }
-
-        public static List<Common.Clases.Categoria> Cateogoria_TraerTodas()
-        {
-            return Dominio.Clases.Categoria.TraerTodas();
-        }
-
-        public static bool Categoria_Eliminar(Common.Clases.Categoria pCategoria)
-        {
-            return Dominio.Clases.Categoria.Eliminar(pCategoria);
-        }
-
-        public static Common.Clases.Categoria Cateogoria_TraerEspecifica(Common.Clases.Categoria pCategoria)
-        {
-            return Dominio.Clases.Categoria.TraerEspecifica(pCategoria);
-        }
-
-        public static bool Categoria_Modificar(Common.Clases.Categoria pCategoria)
-        {
-            return Dominio.Clases.Categoria.Modificar(pCategoria);
-        }
-
-        #endregion
+      
     }
 }
