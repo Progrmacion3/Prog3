c655df0d562aa6467a9692036e59c5fbc350460f
tomas0527 <tomasjorcin@gmail.com>
2021-06-22 15:40:28 -0300

    Agregación de un identificador en camionero y la clase parada en el common

--- Common/Clases/Camionero.cs
+++ Common/Clases/Camionero.cs
@@ -8,6 +8,7 @@ namespace Common.Clases
 {
     class Camionero
     {
+        public int identificadorCam { get; set; }
         public int Edad { get; set; }
         public string Tipo_Libreta { get; set; }
         public DateTime Fecha_Vencimiento_Libreta { get; set; }
