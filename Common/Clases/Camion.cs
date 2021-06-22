6dc4858965b0c5eda9e34301673f3e0d32caf10d
tomas0527 <tomasjorcin@gmail.com>
2021-06-22 14:53:29 -0300

    Estructura de atributos en common recortada

--- Common/Clases/Camion.cs
+++ Common/Clases/Camion.cs
@@ -7,33 +7,11 @@ namespace Common.Clases
 {
     class Camion
     {
-        private string _marca;
-        private string _modelo;
-        private string _matricula;
-        private int _año;
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Matricula { get; set; }
+        public int Año { get; set; }
 
-        public string Marca
-        {
-            get { return _marca; }
-            set { _marca = value; }
-        }
-
-        public string Modelo
-        {
-            get { return _modelo; }
-            set { _modelo = value; }
-        }
-
-        public string Matricula
-        {
-            get { return _matricula; }
-            set { _matricula = value; }
-        }
-        public int Año
-        {
-            get { return _año; }
-            set { _año = value; }
-        }
-      
+       
     }
 }
--- Common/Clases/Empleado.cs
+++ Common/Clases/Empleado.cs
@@ -7,44 +7,13 @@ namespace Common.Clases
 {
     class Empleado
     {
-        private string _nombre;
-        private string _apellido;
-        private int _ci;
-        private string _tipo;
-        private string _usuario;
-        private string _contraseña;
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int CI { get; set; }
+        public string Tipo { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
 
-        public string Nombre
-        {
-            get { return _nombre; }
-            set { _nombre = value; }
-        }
-
-        public string Apellido
-        {
-            get { return _apellido; }
-            set { _apellido = value; }
-        }
-
-        public int CI
-        {
-            get { return _ci; }
-            set { _ci = value; }
-        }
-        public string Tipo
-        {
-            get { return _tipo; }
-            set { _tipo = value; }
-        }
-        public string Usuario
-        {
-            get { return _usuario; }
-            set { _usuario = value; }
-        }
-        public string Contraseña
-        {
-            get { return _contraseña; }
-            set { _contraseña = value; }
-        }
+       
     }
 }
