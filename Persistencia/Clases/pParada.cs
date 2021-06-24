9fb59f66314d58545dadd71c099f24fa7fcb9eae
tomas0527 <tomasjorcin@gmail.com>
2021-06-24 13:05:01 -0300

    Creacion de clases en Dominio y Persistencia

---
+++ Persistencia/Clases/pParada.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistencia.Clases
+{
+    public class pParada:Persistencia
+    {
+    }
+}
---
+++ Persistencia/Clases/pViaje.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Persistencia.Clases
+{
+    public class pViaje:Persistencia
+    {
+        public static bool AgregarViaje(Common.Clases.Viaje pVia)
+        {
+            bool retorno = true;
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Agregar_Viaje", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@idCamionero",pVia.Camionero.identificadorCam));
+                cmd.Parameters.Add(new SqlParameter("@matriculaCamion",pVia.Camion.Matricula));
+                cmd.Parameters.Add(new SqlParameter("@TipoCargaViaje",pVia.Tipo_Carga));
+                cmd.Parameters.Add(new SqlParameter("@KilajeViaje",pVia.Kilaje));
+                cmd.Parameters.Add(new SqlParameter("@OrigenViaje",pVia.Origen));
+                cmd.Parameters.Add(new SqlParameter("@DestinoViaje",pVia.Destino));
+                cmd.Parameters.Add(new SqlParameter("@FechaInicioViaje",pVia.Fecha_Inicio));
+                cmd.Parameters.Add(new SqlParameter("@FechaFinalizacionViaje",pVia.Fecha_Finalizacion));
+                cmd.Parameters.Add(new SqlParameter("@estadoViaje",pVia.EstadoViaje));
+
+                int rtn = cmd.ExecuteNonQuery();
+
+                if(rtn <= 0)
+                {
+                    retorno = false;
+                }
+
+                if(conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return retorno;
+        }
+    }
+}
