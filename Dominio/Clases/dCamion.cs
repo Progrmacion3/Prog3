17c4f8faf1219994949597a45f4e9ac686f051aa
FabricioV <fabricionicolasviera@hotmail.com>
2021-06-26 14:28:30 -0300

    dCamion

--- Dominio/Clases/dCamion.cs
+++ Dominio/Clases/dCamion.cs
@@ -7,5 +7,19 @@ namespace Dominio.Clases
 {
     public class dCamion
     {
+        public static bool Agregar_Camion(Common.Clases.Camion pCamion)
+        {
+            return Persistencia.Clases.pCamion.AgregarCamion(pCamion);
+        }
+
+        public static bool Eliminar_Camion(Common.Clases.Camion pCamion)
+        {
+            return Persistencia.Clases.pCamion.EliminarCamion(pCamion);
+        }
+
+        public static bool Modificar_Camion(Common.Clases.Camion pCamion)
+        {
+            return Persistencia.Clases.pCamion.ModificarCamion(pCamion);
+        }
     }
 }
