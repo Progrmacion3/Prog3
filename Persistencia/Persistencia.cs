9fa997dba3cfc4941997cf523f3a177ef8375d03
aldilop <80060687+aldilop@users.noreply.github.com>
2021-06-26 14:41:28 -0300

    Final amargo sólo queda hoy

--- Persistencia/Persistencia.cs
+++ Persistencia/Persistencia.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace Persistencia
 {
-    public class Persistencia
+    public abstract class Persistencia
     {
         protected static string CadenaDeConexion
         {
-            get
-            {
-                return ConfigurationManager.ConnectionStrings["WindowsAuth"].ToString(); 
-                //@"Server=WINDOWS-LHK6TF8\ESTEBANM;Database=Ejemplo;User Id=sa; password=@MartinezAnchen;";
-            }
+            get { return ConfigurationManager.ConnectionStrings["WindowsAuth"].ToString(); }
         }
     }
 }
