51c450506509bf9065af736427885ec805027b7f
FabricioV <fabricionicolasviera@hotmail.com>
2021-06-25 23:47:23 -0300

    pCamionero

--- Persistencia/Clases/pCamionero.cs
+++ Persistencia/Clases/pCamionero.cs
@@ -9,5 +9,116 @@ namespace Persistencia.Clases
 {
     public class pCamionero:Persistencia
     {
-    }
+            public static bool AgregarCamionero(Common.Clases.Camionero pCamionero)
+            {
+                bool retorno = true;
+
+                try
+                {
+                    var conn = new SqlConnection(CadenaDeConexion);
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("Agregar_Camionero", conn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@EdadCamionero", pCamionero.Edad));
+                cmd.Parameters.Add(new SqlParameter("@tipoLibretaCamionero", pCamionero.Tipo_Libreta));
+                cmd.Parameters.Add(new SqlParameter("@fechaVencimientoLibreta", pCamionero.Fecha_Vencimiento_Libreta));
+
+
+                int rtn = cmd.ExecuteNonQuery();
+
+                    if (rtn <= 0)
+                    {
+                        retorno = false;
+                    }
+
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return retorno;
+            }
+
+
+
+
+            public static bool ModificarCamionero(Common.Clases.Camionero pCamionero)
+            {
+                bool retorno = true;
+
+                try
+                {
+                    var conn = new SqlConnection(CadenaDeConexion);
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("Modificar_Camionero", conn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(new SqlParameter("@EdadCamionero", pCamionero.Edad));
+                    cmd.Parameters.Add(new SqlParameter("@tipoLibretaCamionero", pCamionero.Tipo_Libreta));
+                    cmd.Parameters.Add(new SqlParameter("@fechaVencimientoLibreta", pCamionero.Fecha_Vencimiento_Libreta));
+
+                int rtn = cmd.ExecuteNonQuery();
+
+                    if (rtn <= 0)
+                    {
+                        retorno = false;
+                    }
+
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return retorno;
+            }
+
+
+            public static bool EliminarCamionero(Common.Clases.Camionero pCamionero)
+            {
+                bool retorno = true;
+
+                try
+                {
+                    var conn = new SqlConnection(CadenaDeConexion);
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("Eliminar_Camionero", conn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(new SqlParameter("@idViaje", pCamionero.identificadorCam));
+
+                    int rtn = cmd.ExecuteNonQuery();
+
+                    if (rtn <= 0)
+                    {
+                        retorno = false;
+                    }
+
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return retorno;
+            }
+        }
 }
+
