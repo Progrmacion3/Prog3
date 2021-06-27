900339b63c683f2252fb676038c1a1a01b25fa49
ExtraUk <pablo.lignelli@hotmail.com>
2021-06-27 18:10:54 -0300

    Agregar Camion a BD

---
+++ Obligatorio/obligatorio/Persistencia/Clases/Persistencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace obligatorio.Persistencia.Clases
+{
+    public class Persistencia
+    {
+        protected static string CadenaDeConexion
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["SqlServerAuth"].ToString();
+                //@"Server=WINDOWS-LHK6TF8\ESTEBANM;Database=obligProgramacion;User Id=sa; password=@MartinezAnchen;";
+            }
+        }
+    }
+}
---
+++ Obligatorio/obligatorio/Persistencia/Clases/pCamion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using obligatorio.Dominio;
+
+namespace obligatorio.Persistencia.Clases
+{
+    public class pCamion : Persistencia
+    {
+        public static bool AgregarCamion(Camion pCamion)
+        {
+            bool retorno = true;
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Camion_Agregar", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@marca", pCamion.Marca));
+                cmd.Parameters.Add(new SqlParameter("@modelo", pCamion.Modelo));
+                cmd.Parameters.Add(new SqlParameter("@ano", pCamion.Ano));
+                cmd.Parameters.Add(new SqlParameter("@matriculaCamion", pCamion.Matricula));
+
+                int rtn = cmd.ExecuteNonQuery();
+
+                if (rtn <= 0)
+                {
+                    retorno = false;
+                }
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return retorno;
+        }
+    }
+}
