8990bfd559f6d690cb582e8e0b5b6b4b3f9925a1
ExtraUk <pablo.lignelli@hotmail.com>
2021-06-27 17:43:24 -0300

    Funcionalidades Camiones + Extras

--- Obligatorio/obligatorio/Presentacion/Camiones.aspx.cs
+++ Obligatorio/obligatorio/Presentacion/Camiones.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using obligatorio.Dominio;
 
 namespace obligatorio.Presentacion
 {
@@ -12,6 +13,39 @@ namespace obligatorio.Presentacion
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void ListarDatos()
+        {
+
+        }
+        private void LimpiarCampos()
+        {
+            this.InputMarca.Text = "";
+            this.InputModelo.Text = "";
+            this.InputMatricula.Text = "";
+            this.InputAno.Text = "";
+        }
+
+        protected void btnAlta_Click(object sender, EventArgs e)
+        {
+            Empresa empresa = new Empresa();
+            string marca = this.InputMarca.Text;
+            string modelo = this.InputModelo.Text;
+            string matricula = this.InputMatricula.Text;
+            int ano = int.Parse(this.InputAno.Text);
+            Camion camion = new Camion(marca, modelo, matricula, ano);
+
+            if (empresa.MenuCamion("alta", camion))
+            {
+                this.ListarDatos();
+                this.LimpiarCampos();
+            }
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.LimpiarCampos();
         }
     }
 }
