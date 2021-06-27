b556e9868f1f79dd2f5bdc1bfafb5d2cf84cb1b0
Zellon19 <axel.alvarez2k4@outlook.es>
2021-06-27 17:29:06 -0300

    viajes

---
+++ Obligatorio/obligatorio/Presentacion/Viajes.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Viajes.aspx.cs" Inherits="obligatorio.Presentacion.Viajes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Gestión viajes" Font-Size="Larger"></asp:Label>
+    <br /> <br />
+
+    <asp:Label ID="Label2" runat="server" Text="Camionero:"></asp:Label>
+    <asp:TextBox ID="txtCamionero" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Text="Camión:"></asp:Label>
+    <asp:TextBox ID="txtCamion" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="Carga:"></asp:Label>
+    <asp:TextBox ID="txtCarga" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label5" runat="server" Text="Kilaje:"></asp:Label>
+    <asp:TextBox ID="txtKilaje" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label6" runat="server" Text="Origen:"></asp:Label>
+    <asp:TextBox ID="txtOrigen" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label7" runat="server" Text="Destino:"></asp:Label>
+    <asp:TextBox ID="txtDestino" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label8" runat="server" Text="Fecha de inicio:"></asp:Label>
+    <asp:Calendar ID="dtpFechaInicio" runat="server"></asp:Calendar>
+    <br />
+    <asp:Label ID="Label9" runat="server" Text="Fecha de finalización"></asp:Label>
+    <asp:Calendar ID="dtpFechaFin" runat="server"></asp:Calendar>
+    <br />
+    <asp:Label ID="Label10" runat="server" Text="Estado:"></asp:Label>
+    <asp:TextBox ID="txtEstado" runat="server"></asp:TextBox>
+    <br />
+
+    <div>
+        <br />
+        <br />
+        <asp:Button ID="btnAlta" Text="Alta" runat="server" OnClick="btnAlta_Click"/>
+        <asp:Button ID="btnBaja" Text="Baja" runat="server" OnClick="btnBaja_Click"/>    
+        <asp:Button ID="btnModificar" Text="Modificar" runat="server" OnClick="btnModificar_Click"/>
+        <asp:Button ID="btnLimpiar" Text="Limpiar" runat="server" OnClick="btnLimpiar_Click"/>
+    </div>
+
+</asp:Content>
---
+++ Obligatorio/obligatorio/Presentacion/Viajes.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using obligatorio.Dominio;
+
+namespace obligatorio.Presentacion
+{
+    public partial class Viajes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListarDatos();
+            }
+        }
+
+        private bool FaltanDatos() // comprobamos que todos los campos tengan datos y las fechas sean validas
+        {
+            if (this.txtCamion.Text == "" || this.txtCamionero.Text == "" || this.txtCarga.Text == "" || this.txtDestino.Text == "" || this.txtEstado.Text == "" || this.txtKilaje.Text == "" || this.txtOrigen.Text == "" || this.dtpFechaFin.SelectedDate <= DateTime.Today.Date || this.dtpFechaInicio.SelectedDate < DateTime.Today.Date || this.dtpFechaFin.SelectedDate == this.dtpFechaInicio.SelectedDate)
+                return false;
+            return true;
+        }
+        private void LimpiarCampos()
+        {
+            this.txtCamion.Text = "";
+            this.txtCamionero.Text = "";
+            this.txtCarga.Text = "";
+            this.txtDestino.Text = "";
+            this.txtEstado.Text = "";
+            this.txtKilaje.Text = "";
+            this.txtOrigen.Text = "";
+            this.dtpFechaFin.SelectedDate = DateTime.Today.Date;
+            this.dtpFechaInicio.SelectedDate = DateTime.Today.Date;
+            return;
+        }
+        private void ListarDatos()
+        {
+            return;
+        }
+
+        protected void btnAlta_Click(object sender, EventArgs e)
+        {
+            if (!FaltanDatos())
+            {
+                Empresa mEmpresa = new Empresa();
+                int mIdCamionero = int.Parse(this.txtCamionero.Text);
+                int mIdCamion = int.Parse(this.txtCamion.Text);
+                string mCarga = this.txtCarga.Text;
+                int mKilaje = int.Parse(this.txtKilaje.Text);
+                string mOrigen = this.txtOrigen.Text;
+                string mDestino = this.txtDestino.Text;
+                DateTime mFechaInicio = this.dtpFechaInicio.SelectedDate;
+                DateTime mFechaFin = this.dtpFechaFin.SelectedDate;
+                string mEstado = this.txtEstado.Text;
+                Camion elCamion = mEmpresa.BuscarCamion(new Camion(mIdCamion));
+                Camionero elCamionero = mEmpresa.BuscarCamionero(new Camionero(mIdCamionero));
+                Viaje unViaje = new Viaje(elCamionero, elCamion, mCarga, mKilaje, mOrigen, mDestino, mFechaInicio, mFechaFin, mEstado);
+                if(mEmpresa.MenuViaje("alta", unViaje))
+                {
+                    this.LimpiarCampos();
+                    this.ListarDatos();
+                    //mandar mensaje al usuario o algo indicando que se agrego no se xd
+                    return;
+                }
+                //mandar mensaje indicando un error
+                return;
+            }
+            //avisar que faltan datos
+            return;
+        }
+
+        protected void btnBaja_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
