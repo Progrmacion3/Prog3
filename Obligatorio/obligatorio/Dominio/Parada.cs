075b91f18f27a2c07987f345dfc4bdeb2e293e1a
Zellon19 <axel.alvarez2k4@outlook.es>
2021-06-28 20:41:04 -0300

    parada/s

--- Obligatorio/obligatorio/Dominio/Parada.cs
+++ Obligatorio/obligatorio/Dominio/Parada.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using obligatorio.Persistencia.Clases;
 
 namespace obligatorio.Dominio
 {
@@ -27,11 +28,37 @@ namespace obligatorio.Dominio
             get { return _comentario; }
             set { _comentario = value; }
         }
-
         public int IdViaje
         {
             get { return _idViaje; }
             set { _idViaje = value; }
+        }
+
+        public bool AgregarParada(Parada parParada)
+        {
+            if (this.BuscarParada(parParada) == null && pParada.AgregarParada(parParada))
+                return true;
+            return false;
+
+        }
+        public bool EliminarParada(Parada parParada)
+        {
+            if (this.BuscarParada(parParada) != null && pParada.EliminarParada(parParada))
+                return true;
+            return false;
+        }
+        public bool ModificarParada(Parada parParada)
+        {
+            if (this.BuscarParada(parParada) != null && pParada.ModificarParada(parParada))
+                return true;
+            return false;
+        }
+        public Parada BuscarParada(Parada pParada)
+        {
+            foreach (Parada lParada in new Empresa().ListaParadas())
+                if (lParada.Id == pParada.Id)
+                    return lParada;
+            return null;
         }
 
         public Parada(int pId, string pRazon, string pComentario, int pIdViaje)
@@ -40,6 +67,21 @@ namespace obligatorio.Dominio
             Razon = pRazon;
             Comentario = pComentario;
             IdViaje = pIdViaje;
+        }
+        public Parada(string pRazon, string pComentario, int pIdViaje)
+        {
+            Razon = pRazon;
+            Comentario = pComentario;
+            IdViaje = pIdViaje;
+        }
+        public Parada(int pId, int pIdViaje)
+        {
+            Id = pId;
+            IdViaje = pIdViaje;
+        }
+        public Parada()
+        {
+
         }
     }
 }
