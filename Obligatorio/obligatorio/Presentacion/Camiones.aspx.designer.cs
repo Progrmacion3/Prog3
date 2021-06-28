c41f1c9d82ac2441e9551942358878128550ef59
ExtraUk <pablo.lignelli@hotmail.com>
2021-06-28 19:02:16 -0300

    Grid camiones

--- Obligatorio/obligatorio/Presentacion/Camiones.aspx.designer.cs
+++ Obligatorio/obligatorio/Presentacion/Camiones.aspx.designer.cs
@@ -76,15 +76,6 @@ namespace obligatorio.Presentacion {
         protected global::System.Web.UI.WebControls.Button btnAlta;
         
         /// <summary>
-        /// btnBaja control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnBaja;
-        
-        /// <summary>
         /// btnMod control.
         /// </summary>
         /// <remarks>
@@ -101,6 +92,24 @@ namespace obligatorio.Presentacion {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLimpiar;
+        
+        /// <summary>
+        /// grdCamiones control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdCamiones;
+        
+        /// <summary>
+        /// Test1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource Test1;
         
         /// <summary>
         /// lblDataOutput control.
