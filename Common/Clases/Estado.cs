435c72483babf529114e28c3597160f3cd4d9f79
BrunoPalermo23 <brunopalermo023@hotmail.com>
2021-06-28 20:51:10 -0300

    Clase Estado, pEstado

---
+++ Common/Clases/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Clases
+{
+    public class Estado
+    {
+        public int Identificador { get; set; }
+        public string Estados { get; set; }
+
+        public string Comentario { get; set; }
+    }
+}
---
+++ Persistencia/Clases/pEstado.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Persistencia.Clases
+{
+    public class pEstado : Persistencia
+    {
+
+        public static bool Agregar(Common.Clases.Estado pEstado)
+        {
+            bool retorno = true;
+
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+                //1. Identeificamos el sotre procedure a ejecutar
+                SqlCommand cmd = new SqlCommand("Estado_Agregar", conn);
+
+                //2. Identificamos el tipo de ejecucion, en este caso un SP
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                //3. En caso de que los lleve se ponen los parametros de SP
+                cmd.Parameters.Add(new SqlParameter("@idEstado", pEstado.Identificador));
+                cmd.Parameters.Add(new SqlParameter("@Estados", pEstado.Estados));
+                cmd.Parameters.Add(new SqlParameter("@Comentario", pEstado.Comentario));
+
+
+                //Ejecutamos el store desde c#
+                int rtn = cmd.ExecuteNonQuery();
+
+                if (rtn <= 0)
+                {
+                    retorno = false;
+                }
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return retorno;
+
+
+
+        }
+
+        public static List<Common.Clases.Estado> TraerTodas()
+        {
+            List<Common.Clases.Estado> retorno = new List<Common.Clases.Estado>();
+            Common.Clases.Estado est;
+
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+                //1. Identificamos el store Procedure a ejecutar
+                SqlCommand cmd = new SqlCommand("Estado_TraerTodas", conn);
+
+                //2. Identificamos el tipo de ejecucion, en este caso un SP
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                //3. Ejecutamos el sotre desde C#
+                using (SqlDataReader oReader = cmd.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        est = new Common.Clases.Estado();
+                        est.Identificador = int.Parse(oReader["idEstado"].ToString());
+                        est.Estados = oReader["Estados"].ToString();
+                        est.Comentario = oReader["Categoria"].ToString();
+                        retorno.Add(est);
+                    }
+
+                    conn.Close();
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return retorno;
+
+
+        }
+
+        public static bool Eliminar(Common.Clases.Estado pEstado)
+        {
+            bool retorno = true;
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+
+                //1. Identificamos el store procedure a ejecutar
+                SqlCommand cmd = new SqlCommand("Estado_Eliminar", conn);
+
+                //2. Identificamos el tipo de ejecucion, en este caso SP
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //3. En caso de que los lleve se ponen los parametros del SP
+                cmd.Parameters.Add(new SqlParameter("@idEstado", pEstado.Identificador));
+                cmd.Parameters.Add(new SqlParameter("@Estados", pEstado.Estados));
+                cmd.Parameters.Add(new SqlParameter("@Comentario", pEstado.Comentario));
+
+                // Ejecutamos el store desde C#
+
+                int rtn = cmd.ExecuteNonQuery();
+
+                if (rtn <= 0)
+                {
+                    retorno = false;
+                }
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return retorno;
+        }
+
+
+        public static bool Modificar(Common.Clases.Estado pEstado)
+        {
+            bool retorno = true;
+
+            try
+            {
+                var conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+
+
+                //1. Identificamos el store Procedure a ejecutar
+                SqlCommand cmd = new SqlCommand("Estado_Modificar", conn);
+
+                //2. Identificamos el tipo de ejecucion, en este caso un SP
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //3 en caso de que los lleve se ponen los parametros del SP
+                cmd.Parameters.Add(new SqlParameter("@IdEstado", pEstado.Identificador));
+                cmd.Parameters.Add(new SqlParameter("@Estados", pEstado.Estados));
+                cmd.Parameters.Add(new SqlParameter("@Comentario", pEstado.Comentario));
+
+                //Ejecutamos el store desde c#
+                int rtn = cmd.ExecuteNonQuery();
+
+                if (rtn <= 0)
+                {
+                    retorno = false;
+                }
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return retorno;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //public static Common.Clases.Estado TraerEspecifica(Common.Clases.Estado pEstado);
+        // {
+        //     Common.Clases.Estado retorno = null;
+
+        //try
+        //{
+        
+        //    var conn = new SqlConnection(CadenaDeConexion);
+        //    conn.Open();
+
+
+        //    //1. Identificamos el store porcedure a ejecutar
+        //    SqlCommand cmd = new SqlCommand("Estado_TraerEspecifica", conn);
+            
+        ////2. Identificamos el tipo de ejecucion, en este caso es un SP
+        //    cmd.CommandType = CommandType.StoreProcedure;
+
+        //    //3. En caso de que los lleve se ponen los parametros del SP
+        //    cmd.Parameters.Add(new SqlParameters("@Identificador", pEstado.Identificador));
+
+
+        //    //Ejecturamos el sotre desde C#
+
+        //    using(SqlDataReader oReader = cmd.ExecuteReader())
+        //    {
+        //         while (oReader.Read())
+        //            {
+        //                retorno = new Common.Clases.Estado();
+        //                retorno.Identificador = int.Parse(oReader["Identificador"].ToString());
+        //                retorno.Estados = oReader["Estados"].ToString();
+        //                retorno.Comentario = oReader["Comentario"].ToString();
+        //            }
+        //            conn.Close();
+
+        //        }
+        //   }
+        //   catch (Expeption ex)
+        //    {
+        //         throw ex;
+        //    }
+        //        return retorno;
+
+        // }
+
+
+
+
+
+        }
+        
+
+    }
+
