bf11e7521797a5acb8fb78c348c66430bf3ecac5
tomas0527 <tomasjorcin@gmail.com>
2021-06-28 21:43:04 -0300

    grilla de viajes(si hago constantes y cortos es porque cambio del coso del obligatorio para ver algo xd)

--- Ejemplo.Web/Viaje.aspx.designer.cs
+++ Ejemplo.Web/Viaje.aspx.designer.cs
@@ -191,5 +191,23 @@ namespace Ejemplo.Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblResultado;
+        
+        /// <summary>
+        /// btnAgregarViaje control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarViaje;
+        
+        /// <summary>
+        /// grillaViajes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grillaViajes;
     }
 }
