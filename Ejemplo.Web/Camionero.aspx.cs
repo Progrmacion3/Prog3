d0ccc8e1f2a9e0d970e5148ad25fafdaf3b9e2e0
FabricioV <fabricionicolasviera@hotmail.com>
2021-06-30 09:16:03 -0300

    abmCamionero

--- Ejemplo.Web/Camionero.aspx.cs
+++ Ejemplo.Web/Camionero.aspx.cs
@@ -16,22 +16,114 @@ namespace Ejemplo.Web
 
         protected void btnAgregarCamionero_Click(object sender, EventArgs e)
         {
+            Common.Clases.Camionero camionero = new Common.Clases.Camionero();
+            camionero.Edad = int.Parse(this.txtEdadCam.Text);
+            camionero.Fecha_Vencimiento_Libreta =  DateTime.Parse(this.txtFechaVencimientoLibreta.Text);
+            camionero.Tipo_Libreta = this.txtTipoLibreta.Text;
 
+            try
+            {
+                bool resultadoCamionero = Dominio.Fachada.Agregar_Camionero(camionero);
+
+                if (resultadoCamionero)
+                {
+                    lblResultadoCamionero.Text = "Se ha agregado correctamente un camionero";
+                }
+                else
+                {
+                    lblResultadoCamionero.Text = "Error: no se agrego el camionero";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         protected void btnModificarCamionero_Click(object sender, EventArgs e)
         {
+            Common.Clases.Camionero camionero = new Common.Clases.Camionero();
+            camionero.Edad = int.Parse(this.txtEdadCam.Text);
+            camionero.Fecha_Vencimiento_Libreta = DateTime.Parse(this.txtFechaVencimientoLibreta.Text);
+            camionero.Tipo_Libreta = this.txtTipoLibreta.Text;
 
-        }
+            try
+            {
+                bool resultadoCamionero = Dominio.Fachada.Modificar_Camionero(camionero);
+
+                if (resultadoCamionero)
+                {
+                    lblResultadoCamionero.Text = "Se ha agregado correctamente un camionero";
+                }
+                else
+                {
+                    lblResultadoCamionero.Text = "Error: no se agrego el camionero";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+    }
 
         protected void grillaCamioneros_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
+            try
+            {
+                this.lblResultadoCamionero.Text = string.Empty;
+
+                TableCell celdaIdcamionero = grillaCamioneros.Rows[e.RowIndex].Cells[1];
+                Common.Clases.Camionero camionero = new Common.Clases.Camionero();
+                camionero.identificadorCam = int.Parse(celdaIdcamionero.Text);
+
+                bool resultado = Dominio.Fachada.Eliminar_Camionero(camionero);
+
+                if (resultado)
+                {
+                    ClientScript.RegisterClientScriptBlock(GetType(), "alert", "alert('Camionero eliminado exitosamente')", true);
+
+                }
+
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(GetType(), "alert", "alert('Error: no se pudo eliminar camionero')", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
         }
 
         protected void grillaCamioneros_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
         {
+            try
+            {
+                this.lblResultadoCamionero.Text = string.Empty;
 
+                TableCell celdaIdcamionero = grillaCamioneros.Rows[e.NewSelectedIndex].Cells[1];
+                Common.Clases.Camionero camionero = new Common.Clases.Camionero();
+                camionero.identificadorCam = int.Parse(celdaIdcamionero.Text);
+                camionero = Dominio.Fachada.
+
+
+            if (camionero != null)
+                {
+                    this.txtEdadCam.Text = camionero.Edad.ToString();
+                    this.txtFechaVencimientoLibreta.Text = camionero.Fecha_Vencimiento_Libreta.ToString();
+                    this.txtTipoLibreta.Text = camionero.Tipo_Libreta;
+                    
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(GetType(), "alert", "alert('Error: no se pudo eliminar')", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
