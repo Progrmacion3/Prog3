4d0d65100660bd9045f54fa708c116c30b174780
FabricioV <fabricionicolasviera@hotmail.com>
2021-06-30 20:27:48 -0300

    actualizar P

--- Ejemplo.Web/ParadaC.aspx.cs
+++ Ejemplo.Web/ParadaC.aspx.cs
@@ -16,12 +16,58 @@ namespace Ejemplo.Web
 
         protected void btnActualizarParada_Click(object sender, EventArgs e)
         {
+            Common.Clases.Parada paradaC = new Common.Clases.Parada();
+            paradaC.Comentario = this.txtComentarioCam.Text;
+            paradaC.EstadoParada = this.txtEstadoParadaC.Text;
+            paradaC.Tipo = this.txtTipoParadaC.Text;
 
+            try
+            {
+                bool resultadoParada = Dominio.Fachada.Modificar_Parada(paradaC);
+
+                if (resultadoParada)
+                {
+                    lblResultadoParadaC.Text = "Se ha agregado correctamente una parada";
+                }
+                else
+                {
+                    lblResultadoParadaC.Text = "Error: no se agrego la parada";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         protected void grillaParadas_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
         {
+            try
+            {
+                this.lblResultadoParadaC.Text = string.Empty;
 
+                TableCell celdaId = grillaParadas.Rows[e.NewSelectedIndex].Cells[1];
+                Common.Clases.Parada paradaC = new Common.Clases.Parada();
+                paradaC.identificadorPar = int.Parse(celdaId.Text);
+                paradaC = Dominio.Fachada.
+
+
+            if (paradaC != null)
+                {
+                    this.txtComentarioCam.Text = paradaC.Comentario;
+                    this.txtEstadoParadaC.Text = paradaC.EstadoParada;
+                    this.txtTipoParadaC.Text = paradaC.Tipo;
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(GetType(), "alert", "alert('Error: no se pudo eliminar')", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
+
     }
 }
